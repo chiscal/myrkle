51e9b032583778ba246a0bd1158465cfe2d97e64
Princewill Olaiya <princewillolaiya@gmail.com>
2023-08-03 12:58:18 +0100

    sort best offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import concurrent.futures
+
 
 import time
 from fastapi import FastAPI,  WebSocket, WebSocketDisconnect
@@ -43,25 +45,54 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 @app.websocket("/ws/brower_id/{browser_id}")
 async def websocker_notif(websocket: WebSocket, browser_id: str):
     await websocket.accept()
-    while True:
-        message = {
-            browser_id: {
-                "message": "A user has requested you to sign his transaction"
+    try:
+        while True:
+            message = {
+                browser_id: {
+                    "message": "A user has requested you to sign his transaction"
+                }
             }
-        }
-        data = await websocket.receive_text()
-        if data == "send":
-            await websocket.send_json(message)
+            data = await websocket.receive_text()
+            if data == "send":
+                await websocket.send_json(message)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 
 @app.websocket("/ws/balance/{wallet_address}")
@@ -70,14 +101,16 @@ async def get_wallet_balance(websocket: WebSocket, wallet_address: str):
     await websocket.accept()
     try:
         while True:
-            await websocket.send_text("Yamete")
-            time.sleep(5)
-            loop = asyncio.get_running_loop()
-            # test = client.get_balance(wallet_address)
-            loop.run_in_executor(None, lambda: asyncio.run(websocket.send_json(client.get_balance(wallet_address))))
-            # await websocket.send_json(test)
+            data = await websocket.receive_text()
+            if data == "send":
+                loop = asyncio.get_running_loop()
+                # test = client.get_balance(wallet_address)
+                loop.run_in_executor(None, lambda: asyncio.run(websocket.send_json(client.get_balance(wallet_address))))
+                # await websocket.send_json(test)
+                time.sleep(5)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 
 @app.websocket("/ws/get-account-order-book-liquidity/{wallet_addr}/{network}/{limit}")
@@ -88,23 +121,25 @@ async def get_account_order_book_liquidity(websocket: WebSocket, wallet_addr: st
     await websocket.accept()
     try:
         while True:
-            await websocket.send_text("Loading")
-            time.sleep(5)
-            loop = asyncio.get_running_loop()
-            loop.run_in_executor(
-                None,
-                lambda: asyncio.run(
-                    websocket.send_json(
-                        client.get_account_order_book_liquidity(wallet_addr, limit)
+            data = await websocket.receive_text()
+            if data == "send":
+                loop = asyncio.get_running_loop()
+                loop.run_in_executor(
+                    None,
+                    lambda: asyncio.run(
+                        websocket.send_json(
+                            client.get_account_order_book_liquidity(wallet_addr, limit)
+                            )
                         )
                     )
-                )
+                time.sleep(5)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 
 @app.websocket("/ws/sort-best-offer/{network}")
-async def get_account_order_book_liquidity(websocket: WebSocket, network: str):
+async def sort_best_offer(websocket: WebSocket, network: str):
     response = {}
     client = XammFinance()
     if network == "testnet":
@@ -134,7 +169,8 @@ async def get_account_order_book_liquidity(websocket: WebSocket, network: str):
                     )
                 time.sleep(5)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 
 @app.websocket("/ws/token-balance/{wallet_addr}/{name}/{issuer_addr}/{network}")
@@ -145,68 +181,88 @@ async def token_balance(websocket: WebSocket, wallet_addr: str, name: str, issue
     await websocket.accept()
     try:
         while True:
-            loop = asyncio.get_running_loop()
-            loop.run_in_executor(
-                None,
-                lambda: asyncio.run(
-                    websocket.send_json(
-                        client.token_balance(
-                                wallet_addr=wallet_addr,
-                                name=name,
-                                issuer_addr=issuer_addr,
+            data = await websocket.receive_text()
+            if data == "send":
+                loop = asyncio.get_running_loop()
+                loop.run_in_executor(
+                    None,
+                    lambda: asyncio.run(
+                        websocket.send_json(
+                            client.token_balance(
+                                    wallet_addr=wallet_addr,
+                                    name=name,
+                                    issuer_addr=issuer_addr,
+                                )
                             )
                         )
                     )
-                )
-            time.sleep(5)
+                time.sleep(5)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 
-@app.websocket("/ws/status/{txid}/{mainnet}/")
+@app.websocket("/ws/status/{txid}/{mainnet}")
 async def status(websocket: WebSocket, txid: str, mainnet: bool):
     client = XammFinance()
     await websocket.accept()
     try:
         while True:
-            loop = asyncio.get_running_loop()
-            loop.run_in_executor(
-                None,
-                lambda: asyncio.run(
-                    websocket.send_json(
-                        client.status(
-                                txid=txid,
-                                mainnet=mainnet,
+            data = await websocket.receive_text()
+            if data == "send":
+                loop = asyncio.get_running_loop()
+                loop.run_in_executor(
+                    None,
+                    lambda: asyncio.run(
+                        websocket.send_json(
+                            client.status(
+                                    txid=txid,
+                                    mainnet=mainnet,
+                                )
                             )
                         )
                     )
-                )
-            time.sleep(5)
+                time.sleep(5)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
 
 
-@app.websocket("/ws/token-exists/{token}/{issuer}/")
+@app.websocket("/ws/token-exists/{token}/{issuer}")
 async def status(websocket: WebSocket, token: str, issuer: str):
     client = XammFinance()
     await websocket.accept()
     try:
         while True:
-            loop = asyncio.get_running_loop()
-            loop.run_in_executor(
-                None,
-                lambda: asyncio.run(
-                    websocket.send_json(
-                        client.token_exists(
-                                token=token,
-                                issuer=issuer,
+            data = await websocket.receive_text()
+            if data == "send":
+                loop = asyncio.get_running_loop()
+                loop.run_in_executor(
+                    None,
+                    lambda: asyncio.run(
+                        websocket.send_json(
+                            client.token_exists(
+                                    token=token,
+                                    issuer=issuer,
+                                )
                             )
                         )
                     )
-                )
-            time.sleep(5)
+                time.sleep(5)
     except WebSocketDisconnect:
-        print("Client disconnected")
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Disconnected")
+
+
+@app.on_event("startup")
+def startup_event():
+    # instantiate the ThreadPool
+    app.state.pool = concurrent.futures.ThreadPoolExecutor()
+
+@app.on_event("shutdown")
+def shutdown_event():  
+    # terminate the ThreadPool
+    app.state.pool.shutdown()
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
